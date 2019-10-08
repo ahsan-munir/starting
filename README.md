f8d73996c7cf4bd0be5559ac7939de0fcde9b424
ahsan-munir <ahsan3123@gmail.com>
2019-10-08 19:24:51 +0500

    Step1: Styles Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wordpress Theme Startup
+Wordpress Starter Theme with Automated Tasks for Styles and Scripts (WP with Gulp.js)
+# Styles
+All scss files are added using ITCSS standards with grid layout
---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php get_template_part('template-parts/social-media-tags') ?>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class() ?>>
